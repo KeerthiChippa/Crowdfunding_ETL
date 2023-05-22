fea4dae233c7eee938cc7d83f197089da6af093c
Barani1985GH <121493809+Barani1985GH@users.noreply.github.com>
2023-05-22 21:38:30 +1000

    Add files via upload

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,59 @@
+DROP TABLE IF EXISTS contacts CASCADE;
+
+
+CREATE TABLE contacts (
+	contact_id INT NOT NULL PRIMARY KEY,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50)
+	
+  );
+  
+DROP TABLE IF EXISTS categories CASCADE;
+
+CREATE TABLE categories (
+	category VARCHAR(50),
+	category_id VARCHAR(10) NOT NULL PRIMARY KEY
+	);
+
+DROP TABLE IF EXISTS subcategories CASCADE;
+
+CREATE TABLE subcategories (
+	subcategory VARCHAR(50),
+	subcategory_id VARCHAR(10) NOT NULL PRIMARY KEY
+	
+);
+
+DROP TABLE IF EXISTS campaign CASCADE;
+
+CREATE TABLE campaign(
+	cf_id INT NOT NULL PRIMARY KEY,
+	contact_id INT,
+	company_name VARCHAR(70),
+	desription VARCHAR(100),
+	goal DOUBLE PRECISION,
+	pledged DOUBLE PRECISION,
+	outcome VARCHAR(15),
+	backers_count INT,
+	country VARCHAR(10),
+	currency VARCHAR(10),
+	launched_date DATE,
+	end_date DATE,
+	staff_pick BOOLEAN,
+	spotlight BOOLEAN,
+	category_id VARCHAR(10),
+	subcategory_id VARCHAR(10),
+	FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
+	FOREIGN KEY (category_id) REFERENCES categories(category_id),
+	FOREIGN KEY (subcategory_id) REFERENCES subcategories(subcategory_id)
+);
+
+
+select * from campaign;
+select * from categories;
+select * from subcategories;
+select * from contacts;
+
+
+
+
